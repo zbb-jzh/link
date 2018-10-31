f7c699a3758cbfc4e5f3431feb8e3702f3f6f5f0
zbb-jzh <603845887@qq.com>
2018-10-31 08:24:01 +0800

    1031

--- link/src/com/future/link/consumer/model/_ModelGenerator.java
+++ link/src/com/future/link/consumer/model/_ModelGenerator.java
@@ -4,6 +4,7 @@ import com.future.link.utils.ModelGeneratorUtil;
 
 public class _ModelGenerator {
 	public static void main(String[] args) {
+		
         String tableHead="consumer";
         String moduleName="link";
         ModelGeneratorUtil.start(tableHead,moduleName);
